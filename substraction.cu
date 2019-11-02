00f78b2452136428879b7c4b5c4b51d49c1b9de8
TerenceLiu98 <t_lau@uicstat.com>
2019-11-03 01:34:35 +0800

    substraction

---
+++ substraction.cu
@@ -0,0 +1,69 @@
+/* 
+Basically, the substraction are the same to the `add.cu`
+But be care, DO NOT use `+` to do the substraction
+please use `-`! (JUST FOR FUN) :)
+*/
+
+#include <stdio.h>
+#include "cuda_runtime.h"
+
+// CUDA Kernel Function 
+
+__global__ void add(int *a, int *b, int *c){
+    int i = threadIdx.x;
+    c[i] = b[i] + a[i];
+}
+
+// main Function
+
+int main(){
+    // define A, B, and C
+    // These are three array and we will do A + B = C
+    int A[5] = {1, 2, 3, 4, 5};
+    int B[5] = {7, 8, 10, 18, 20};
+    int C[5];
+    int C_check[5];
+
+    // define arrays for A, B, and C 
+    // and copy these memory from host memory to device memory
+    int *A_gpu;
+    int *B_gpu;
+    int *C_gpu;
+
+    int size = 5 * sizeof(int); 
+
+    // allocate memory for A, B, and C
+    cudaMalloc((void **)&A_gpu, size);
+    cudaMalloc((void **)&B_gpu, size);
+    cudaMalloc((void **)&C_gpu, size);
+
+    // copy the memory
+    cudaMemcpy(A_gpu, A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_gpu, B, size, cudaMemcpyHostToDevice);
+
+    add<<<1, 5>>>(A_gpu, B_gpu, C_gpu);
+
+    // copy memory from device to host 
+    // since the result is stored in C_gpu
+    // we just need this memory
+    cudaMemcpy(C, C_gpu, size, cudaMemcpyDeviceToHost);
+
+
+
+    int i;
+    
+    for (i = 0; i < 5; i++){
+        C_check[i] = A[i] - B[i];
+    }
+    
+    
+    for (i = 0; i < 5; i++){
+        printf("The Sum is %d\n", C[i] == C_check[i]);
+    }
+
+    cudaFree(A_gpu);
+    cudaFree(B_gpu);
+    cudaFree(C_gpu);
+
+    return 0;
+}
